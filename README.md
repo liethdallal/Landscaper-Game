f68c8b6ee1a4a31eb390c686c814e61d56fa31ed
liethdallal <liethdallal@gmail.com>
2023-08-29 13:10:53 -0500

    Game Setup complete

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Landscaper-Game
 A clicker game where the user needs to acquire a certain amount of points to win!
+
+I created this game using HTML, CSS, and Javascript. The javascript used was a way to practice using DOM manipulation, functions, variables, data structures, conditional statements, loops, objects, event handlers, scopes, string manipulation, and boolean logic in javascript.
+
+Skill List and demonstrations: 
+
+-Variables and Objects: The user object holds the player's totalMoney. Other variables (useTeeth, money, etc.) are used to store references to DOM elements.
+
+-DOM Manipulation: The code interacts with the Document Object Model (DOM) using document.querySelector to select and manipulate HTML elements.
+
+-Event Handling: The code uses event listeners (addEventListener) to respond to user interactions (button clicks).
+
+-Functions: Functions like updateMoney, buyItem, and useItem encapsulate specific functionalities, making the code modular.
+
+-Conditional Statements: Conditional statements (if, if...else) control the game's flow based on conditions, like when to display alerts or buttons.
+
+-Loops: The code employs forEach loops to iterate over arrays (gameItems) and attach event listeners to corresponding buttons.
+
+-String Manipulation: The code constructs CSS class names dynamically using template literals (. + item.name).
+
+-Alerts: The alert function is used to provide messages to the player at specific points in the game.
+
+-Boolean Logic: Boolean variables (alertStatusForS, alertStatusForL, etc.) are used to track whether alerts have been shown to avoid repetition.
+
+-Updating HTML Content: The textContent property is used to update the displayed player's total money.
+
+-Game Logic: The code implements game mechanics such as earning money, buying items, and reaching milestones.
+
+-Event Delegation: Event listeners are attached to parent elements (like .button-container and .buy-options) to manage button clicks efficiently.
+
+-Global Variables: Variables like gameOver are used to control the game's state.
+
+-Encapsulation and Abstraction: The code separates concerns by encapsulating functionalities into functions and abstracting away complex operations.
+
+-Responsive Design: The use of responsive units (em, %) in CSS and the viewport meta tag in HTML ensures the game is displayed well on various devices.
+
+-Styling: The game's styling is handled through a separate CSS file (style.css).
---
+++ script.js
@@ -0,0 +1,230 @@
+let user = {
+    totalMoney: 0
+}
+
+
+const useTeeth = document.querySelector('.teeth');
+//the use teeth button 
+
+
+const money = document.querySelector('.money');
+//the dollar digit that is dispalyed on top 
+
+
+
+
+
+const useScissors = document.querySelector('.scissors')
+//The Use Scissors button 
+
+const buyScissors = document.querySelector('.buy-scissor')
+//The buy the siccors option with button nested in it 
+
+const buyButtonS = document.querySelector('.buy')
+//The button for the buy siccors
+
+
+
+
+
+const useLawnmower = document.querySelector('.lawnmower')
+//The use button for lawnmower 
+
+const buyLawnmower = document.querySelector('.buy-lawnmower')
+//The buy lawnmower option with button nested in it 
+
+const buyButtonL = document.querySelector('.buyl')
+//The buy lawnmower button
+
+
+
+
+const usePlawnmower = document.querySelector('.plawnmower')
+//The use button for powered lawnmower
+
+const buyPlawnmower = document.querySelector('.buy-poweredmower')
+//The buy Powered Lawnmower button nested in it 
+
+const buyButtonP = document.querySelector('.buypl')
+//The buy lawnmower button 
+
+
+
+
+const useStudents = document.querySelector('.students')
+//The use button for starving students 
+
+const buyStudents = document.querySelector('.buy-students')
+//The buy starving students button nested in it 
+
+const buyButtonStud = document.querySelector('.buystud')
+
+
+
+
+
+user.totalMoney = 0
+
+function updateMoney () { 
+    money.textContent = user.totalMoney;
+}
+//We can call this function to call money
+
+
+
+
+
+let alertStatusForS = false;
+//used for no repition of alerts after reaching 5
+let alertStatusForL = false;
+//used for no repition of alert after reachig 25
+let alertStatusForPl = false;
+//used for no repition of alert after reaching 250
+let alertStatusForStudents= false;
+//Used for no repition of alert after reaching 500
+
+let gameOver = false;
+
+
+
+
+//--------------------------------------------------------------Use Teeth Button
+useTeeth.addEventListener('click', function () {
+    user.totalMoney ++
+    updateMoney()
+
+    if (user.totalMoney >= 5 && !alertStatusForS){
+        alert('You can now buy Siccors! To make $5 per cut!')
+        buyScissors.style.display = 'block'; 
+        alertStatusForS = true;
+    }
+    if (user.totalMoney >= 25 && !alertStatusForL) {
+        alert('You can now buy a push lawnmower!')
+        buyLawnmower.style.display = "block"
+        alertStatusForL = true;
+    }
+
+})
+
+//-------------------------------------------------------------Buying Scissors on the bottom
+buyButtonS.addEventListener('click', function () {
+        useScissors.style.display = 'block';
+        user.totalMoney -= 5;
+        updateMoney()
+        buyScissors.style.display = 'none'
+    })
+
+
+
+
+//-----------------------------------------------------------------Use Scissors Button
+
+useScissors.addEventListener('click', function () {
+    user.totalMoney += 5
+    updateMoney()
+
+    if (user.totalMoney >= 25 && !alertStatusForL) {
+        alert('You can now buy a push lawnmower!')
+        buyLawnmower.style.display = "block"
+        alertStatusForL = true;
+    }
+
+    if(user.totalMoney >= 250 && !alertStatusForPl){
+        alert('You can now buy a powered lawnmower!')
+        buyPlawnmower.style.display = 'block'
+        alertStatusForPl = true;
+    }
+
+    if(user.totalMoney >=500 && !alertStatusForStudents){
+        alert('You can now buy starving students!')
+        buyStudents.style.display = 'block'
+        alertStatusForStudents = true;
+    }
+    if(user.totalMoney >= 1000 && !gameOver) {
+        alert('Congradulations You beat the Game :)')
+        alert('You can contiue if you would like goodbye for now :)')
+        gameOver = true;
+    
+    }
+})
+
+//---------------------------------------------------------Buying Lawnmower Button
+buyButtonL.addEventListener('click', function () {
+    useLawnmower.style.display = 'block'
+    user.totalMoney -= 25;
+    updateMoney()
+    buyLawnmower.style.display = 'none'
+
+})
+
+//--------------------------------------------------------------Use Lawnmower Button
+useLawnmower.addEventListener('click', function () {
+    user.totalMoney += 50;
+    updateMoney()
+
+    if(user.totalMoney >= 250 && !alertStatusForPl){
+        alert('You can now buy a powered lawnmower!')
+        buyPlawnmower.style.display = 'block'
+        alertStatusForPl = true;
+    }
+    if(user.totalMoney >=500 && !alertStatusForStudents){
+        alert('You can now buy starving students!')
+        buyStudents.style.display = 'block'
+        alertStatusForStudents = true;
+    }
+    if(user.totalMoney >= 1000 && !gameOver) {
+        alert('Congradulations You beat the Game :)')
+        alert('You can contiue if you would like goodbye for now :)')
+        gameOver = true;
+    
+    }
+})
+
+//---------------------------------------------------------------Buying Powered Lawnmower
+buyButtonP.addEventListener('click', function () {
+    usePlawnmower.style.display = 'block'
+    user.totalMoney -= 250
+    updateMoney()
+    buyPlawnmower.style.display = 'none'
+})
+
+//------------------------------------------------------------------Use Powered Lawnmower
+usePlawnmower.addEventListener('click', function () {
+    user.totalMoney += 100
+    updateMoney()
+
+    if(user.totalMoney >=500 && !alertStatusForStudents){
+        alert('You can now buy starving students!')
+        buyStudents.style.display = 'block'
+        alertStatusForStudents = true;
+    }
+    if(user.totalMoney >= 1000 && !gameOver) {
+        alert('Congradulations You beat the Game :)')
+        alert('You can contiue if you would like goodbye for now :)')
+        gameOver = true;
+    
+    }
+})
+
+//-------------------------------------------------------------------------Buying Starving Students
+buyStudents.addEventListener('click', function () {
+    useStudents.style.display = 'block'
+    user.totalMoney -= 500
+    updateMoney()
+    buyStudents.style.display = 'none'
+    alert('All you need now is $1000 and You will Win!!')
+})
+
+//------------------------------------------------------------------------------Use Starving Student 
+useStudents.addEventListener('click', function () {
+    user.totalMoney += 250
+    updateMoney()
+
+    if(user.totalMoney >= 1000 && !gameOver) {
+        alert('Congradulations You beat the Game :)')
+        alert('You can contiue if you would like goodbye for now :)')
+        gameOver = true;
+    
+    }
+})
+
